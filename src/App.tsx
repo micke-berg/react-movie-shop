01f3485876ed3875832541e0d93bdf790f2bc148
Micke Berg <micke.berg.dev@gmail.com>
2020-11-10 13:52:55 +0100

    feature 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// feature 1
+import React from "react";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='grid-container'>
+      <header className='App-header'>
+        <a href='/'>React Movie Shop</a>
       </header>
+      <main>Product List</main>
+      <footer>All rights reserved.</footer>
     </div>
   );
-}
+};
 
 export default App;
