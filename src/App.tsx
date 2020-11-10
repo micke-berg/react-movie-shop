e2750c816e26a7baeb04c9f4cb1622b4e2126805
Micke Berg <micke.berg.dev@gmail.com>
2020-11-10 15:46:04 +0100

    create product component and add data.json dummy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Products from "./components/Products/Products";
+import data from "./data.json";
 
 function App() {
+  const [state, setState] = useState({
+    products: data.products,
+  });
   return (
     <div className='grid-container'>
       <header>
         <a href='/'>React Movie Shop</a>
       </header>
-      <main>Product List</main>
+      <main>
+        <div className='content'>
+          <div className='main'>
+            Products
+            <Products products={state.products} />
+          </div>
+          <div className='sidebar'>Cart Items</div>
+        </div>
+      </main>
       <footer>All rights reserved.</footer>
     </div>
   );
