2c7addae8a084aac5708394ca94bd0077ab114b2
Micke Berg <micke.berg.dev@gmail.com>
2020-11-17 12:49:43 +0100

    adminscreen content and api call

--- src/components/screens/AdminScreen.tsx
+++ src/components/screens/AdminScreen.tsx
@@ -1,12 +1,50 @@
-import React from "react";
-import { deleteOrder } from "../../API";
+import React, { useState } from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import axios from "axios";
 
-import IOrder from "../../Interfaces/IOrder";
+import { ORDERS_URL } from "../../config";
+
+// import IOrder from "../../Interfaces/IOrder";
 import Orders from "../Orders/Orders";
 
 const AdminScreen: React.FC = () => {
+  const [orderId, setOrderId] = useState(0);
+
+  const handleChange = (e: any) => {
+    e.preventDefault();
+    console.log("getValue called", e.target.value);
+    setOrderId(e.target.value);
+  };
+
+  async function deleteOrder(orderId: number) {
+    await axios.delete(`${ORDERS_URL}/${orderId}`).then((result: any) => {
+      console.log(result);
+      console.log("order delete succeeded", result.data);
+      setOrderId(0);
+    });
+  }
+
+  const runDeleteOrder = () => {
+    deleteOrder(orderId);
+  };
+
+  console.log("order id", orderId);
   return (
     <div>
+      <header className='admin-header'>
+        <Link to='/'>Home</Link>
+        <div>
+          <input
+            placeholder=' Enter order id...'
+            required
+            value={orderId}
+            onChange={handleChange}
+          />
+          <button onClick={runDeleteOrder} className='button primary'>
+            Delete
+          </button>
+        </div>
+      </header>
       <Orders />
     </div>
   );
